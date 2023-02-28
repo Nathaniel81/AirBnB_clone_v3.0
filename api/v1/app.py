70943cd25055bf50360ea07cbc5f1a250535436f
Nathaniel81 <nathanield356@gmail.com>
2023-02-28 22:23:58 +0300

    Create a handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ connecting to a database and running on the specified host and port.
 It uses the models module and registers the app_views blueprint.
 """
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -24,3 +24,7 @@ if __name__ == "__main__":
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=getenv('HBNB_API_PORT', 5000),
             threaded=True, debug=True)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
