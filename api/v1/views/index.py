e09ba89cf8d5e35e14bdfeef2e0d2bb080167ee7
Nathaniel81 <nathanield356@gmail.com>
2023-02-28 21:00:46 +0300

    Add stats method to retrieve the number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,36 @@
 #!/usr/bin/python3
-"""Defines a Flask route at /status that returns a JSON object containing the string "OK", 
-indicating a successful status, and has a docstring that describes the endpoint."""
+"""Defines a Flask route"""
 
 from flask import jsonify
 from api.v1.views import app_views
 
+from models import storage
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
+
 
 @app_views.route('/status', methods=['GET'])
 def show_status():
-    """An end point to retrive ok status as a response"""
+    """An end point to retrive "OK" status as a response"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', method=['GET'])
+def stats():
+    """Retrieves the number of each objects by type"""
+    cls_mapper = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+    res = {}
+    for k, v in cls_mapper:
+        res[k] = storage.count(v)
+    return jsonify(res)
