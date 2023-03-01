5271a03ad19c4f351fb8edb98ddfdab8e30acb6c
Nathaniel81 <nathanield356@gmail.com>
2023-03-01 14:37:05 +0300

    Create a new view for City objects

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,12 +2,16 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 from models.city import City
-from flask import jsonify
+from flask import jsonify, abort
 
 app_views.route('/states/<state_id>/cities', methods=['GET'])
 def get_cities(state_id=None):
     """ """
     s = storage.get(State, state_id)
-    cities = [c.to_dict() for c in s.cities]
+    if s is None:
+        abort(404, 'Not found')
+    all_cities = storage.all(City)
+    cities = [c.to_dict() for c in all_cities.values() if c.id == state_id]
+    
     return jsonify(cities)
     
